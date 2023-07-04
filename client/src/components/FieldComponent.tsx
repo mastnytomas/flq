8fbeaf04087c0aee77b2c6c5c2ac315e75130f53
mastnyt <mastnyt.00@spst.eu>
2023-07-04 14:53:16 +0200

    hotifx

--- client/src/components/FieldComponent.tsx
+++ client/src/components/FieldComponent.tsx
@@ -312,7 +312,7 @@ const FieldComponent = () => {
               <>
                 <Card title='Guessing player:'>
                   <p>Position: {selectedPlayer.position}</p>
-				  <p>Name lenght: {selectedPlayer.name.length}</p>
+				  <p>Name length: {selectedPlayer.name.length}</p>
                   <label>
                     Enter player's name:
                     <Input
